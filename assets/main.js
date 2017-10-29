50c7a53e05d21fe12eb198c7ae10fdb297f4e139
Lopatiy <noizejke@gmail.com>
2017-10-30 00:23:36 +0200

    environment setup

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+function linearPositioning(index, w) {
+    return {x: w * index, y:0, z: 0}
+}
+
+var elements = 32;
+
+(function (window) {
+    for(let i = 0; i < elements; i ++ ){
+        let pillon = document.createElement('div');
+
+    }
+})(window);
